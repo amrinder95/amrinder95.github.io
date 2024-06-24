8b3378d18072cb8b528fa3a0e256da16d986440f
Amrinder Singh <amrinder995@gmail.com>
2024-06-23 20:06:35 -0400

    setup gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/amrinder95.github.io',
+  assetPrefix: '/amrinder95.github.io/',
+  output: 'export',
+};
 
 export default nextConfig;
